8ecd3b6f9c7a2c85a773e702c4f5ffe5cfc85f7c
gavielskee <gavielskee1@gmail.com>
2023-07-26 18:02:20 -0400

    created an array of the questions and answers.

---
+++ assets/javascript/script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript',
+        answers: [
+            { text: '<javascript>', correct: false},
+            { text: '<scripting>', correct: false},
+            { text: '<js>', correct: false},
+            { text: '<script>', correct: true},
+        ]
+    },
+    {
+        question: 'Choose the correct HTML element for the largest heading:',
+        answers: [
+            { text: '<h6>', correct: false},
+            { text: '<heading>', correct: false},
+            { text: '<head>', correct: false},
+            { text: '<h1>', correct: true},
+        ]
+    },
+    {
+        question: 'What does CSS stand for?',
+        answers: [
+            { text: 'Cascading Style Sheets', correct: true},
+            { text: 'Computer Style Sheets', correct: false},
+            { text: 'Colorful Style Sheets', correct: false},
+            { text: 'Creative Style Sheets', correct: false},
+        ]
+    },
+    {
+        question: 'Which CSS property controls the text size?',
+        answers: [
+            { text: 'text-size', correct: false},
+            { text: 'font-style', correct: false},
+            { text: 'font-size', correct: true},
+            { text: 'text-style', correct: false},
+        ]  
+    }
+];
+
