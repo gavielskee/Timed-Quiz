673fcc66f06a2e5cf063420d4c62894076f2bbd7
gavielskee <gavielskee1@gmail.com>
2023-07-27 23:44:49 -0400

    added a score that is pushed onto the HTML.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,13 @@
+// Getting the necessary elements from the DOM
+const startScreen = document.getElementById('start-screen');
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answers'); 
+const nextButton = document.getElementById('next-btn');
+const startButton = document.getElementById('start-button');
+const quizContainer = document.querySelector('#quiz-container');
+const scoreContainer = document.getElementById('score');
+const gameOverScreen = document.getElementById('game-over');
+
 const questions = [
     {
         question: 'Inside which HTML element do we put the JavaScript',
@@ -38,48 +48,43 @@ const questions = [
 ];
 
 
-
 let timeEl = document.getElementById('time-element');
 let currentTime = 100;
 
-// Getting the necessary elements from the DOM
-const startScreen = document.getElementById('start-screen'); // Fix variable name here
-const questionElement = document.getElementById('question');
-const answerButton = document.getElementById('answers'); // Fix variable name here
-const nextButton = document.getElementById('next-btn');
-const startButton = document.getElementById('start-button');
-const quizContainer = document.querySelector('#quiz-container');
-
 // Event listener for startTimer
-startButton.addEventListener('click', showQuestion);
+startButton.addEventListener('click', startQuiz);
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-const showStartPage = () => {
+function showStartPage(){
   startScreen.style.display = 'block';
   quizContainer.style.display = 'none';
 };
 
-const startQuiz = () => {
+//Function to start quiz
+function startQuiz(){
   currentQuestionIndex = 0;
   score = 0;
   startScreen.style.display = 'none';
   quizContainer.style.display = 'block';
-  showQuestion(currentQuestionIndex);
+  showQuestion();
+  startTimer();
 };
 
-function showQuestion(questionIndex) {
-  const question = questions[questionIndex];
-  questionElement.textContent = question.question;
+function showQuestion() {
+  currentQuestion = questions[currentQuestionIndex];
+  questionElement.innerText = currentQuestion.question;
+  scoreContainer.innerText = score;
 
-  answerButton.innerHTML = ''; // Clear previous answers
+// Clear previous answers
+  answerButton.innerHTML = ''; 
 
-  // Create and display answer buttons
-  question.answers.forEach((answer, index) => {
+//   Create and display answer buttons
+    currentQuestion.answers.forEach(answers => {
     const button = document.createElement('button');
-    button.textContent = answer.text;
-    button.addEventListener('click', () => handleAnswerClick(answer.correct));
+    button.textContent = answers.text;
+    button.addEventListener('click', () => handleAnswerClick(answers.correct));
     answerButton.appendChild(button);
   });
 }
@@ -89,6 +94,7 @@ function handleAnswerClick(isCorrect) {
   if (isCorrect) {
     // Increment score for correct answer
     score++;
+    console.log(score);
   }
 
   // Move to the next question
@@ -119,7 +125,7 @@ function endQuiz() {
   quizContainer.style.display = 'none';
 
   // Display the user's score or any other end-of-quiz actions
-  console.log(`Your score: ${score} out of ${questions.length}`);
+  gameOverScreen.innerText=`Your score: ${score} out of ${questions.length}`;
 }
 
 // Show the start page initially
