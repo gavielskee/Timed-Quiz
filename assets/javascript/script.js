4a29d540c6ef3929c9d9470df82c1b678fe9c238
gavielskee <gavielskee1@gmail.com>
2023-07-27 22:39:15 -0400

    created separate functions for the questions.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -37,3 +37,92 @@ const questions = [
     }
 ];
 
+
+
+let timeEl = document.getElementById('time-element');
+let currentTime = 100;
+
+// Getting the necessary elements from the DOM
+const startScreen = document.getElementById('start-screen'); // Fix variable name here
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answers'); // Fix variable name here
+const nextButton = document.getElementById('next-btn');
+const startButton = document.getElementById('start-button');
+const quizContainer = document.querySelector('#quiz-container');
+
+// Event listener for startTimer
+startButton.addEventListener('click', showQuestion);
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const showStartPage = () => {
+  startScreen.style.display = 'block';
+  quizContainer.style.display = 'none';
+};
+
+const startQuiz = () => {
+  currentQuestionIndex = 0;
+  score = 0;
+  startScreen.style.display = 'none';
+  quizContainer.style.display = 'block';
+  showQuestion(currentQuestionIndex);
+};
+
+function showQuestion(questionIndex) {
+  const question = questions[questionIndex];
+  questionElement.textContent = question.question;
+
+  answerButton.innerHTML = ''; // Clear previous answers
+
+  // Create and display answer buttons
+  question.answers.forEach((answer, index) => {
+    const button = document.createElement('button');
+    button.textContent = answer.text;
+    button.addEventListener('click', () => handleAnswerClick(answer.correct));
+    answerButton.appendChild(button);
+  });
+}
+
+// Function to handle the user's answer click
+function handleAnswerClick(isCorrect) {
+  if (isCorrect) {
+    // Increment score for correct answer
+    score++;
+  }
+
+  // Move to the next question
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion(currentQuestionIndex);
+  } else {
+    // If there are no more questions, end the quiz
+    endQuiz();
+  }
+}
+
+// Starts timer when pressing the button
+function startTimer() {
+  let timeInterval = setInterval(function () {
+    if (currentTime === 0 || currentQuestionIndex >= questions.length) {
+      clearInterval(timeInterval);
+      endQuiz();
+    }
+    timeEl.textContent = currentTime;
+    currentTime--;
+  }, 1000);
+}
+
+// Function to end the quiz
+function endQuiz() {
+  // Hide the quiz container
+  quizContainer.style.display = 'none';
+
+  // Display the user's score or any other end-of-quiz actions
+  console.log(`Your score: ${score} out of ${questions.length}`);
+}
+
+// Show the start page initially
+showStartPage();
+
+
